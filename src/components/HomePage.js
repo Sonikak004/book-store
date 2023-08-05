9f7f8ed2e0195fc414168dd16e326ddcf5909a4b
Sonika Kumar <sonikak004@gmail.com>
2023-08-05 03:45:15 +0100

    Import the book.js to homepage to add input

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,25 +1,53 @@
-import React from 'react';
+// src/components/HomePage.js
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import BookList from './BookList';
-import NewBookForm from './NewBookForm';
-import { addBook } from '../redux/books/booksSlice';
+import { addBook, removeBook } from '../redux/books/booksSlice';
+import Book from './book';
 
 function HomePage() {
-  const dispatch = useDispatch();
-  const books = useSelector((state) => state.books.books);
+  const [newBookTitle, setNewBookTitle] = useState('');
+  const [newBookAuthor, setNewBookAuthor] = useState('');
 
-  const handleAddBook = (newBook) => {
+  const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+
+  const handleAddBook = () => {
+    const newBook = {
+      item_id: `item${books.length + 1}`,
+      title: newBookTitle,
+      author: newBookAuthor,
+      category: 'Unknown',
+    };
     dispatch(addBook(newBook));
+    setNewBookTitle('');
+    setNewBookAuthor('');
+  };
+
+  const handleRemoveBook = (itemId) => {
+    dispatch(removeBook(itemId));
   };
 
   return (
     <div>
       <h1>Welcome to the Book Store</h1>
       <h2>Book List</h2>
-      <BookList books={books} />
+      {books.map((book) => (
+        <div key={book.item_id}>
+          <Book title={book.title} author={book.author} category={book.category} />
+          <button type="submit" onClick={() => handleRemoveBook(book.item_id)}>
+            Remove
+          </button>
+        </div>
+      ))}
 
       <h2>Add a New Book</h2>
-      <NewBookForm onAddBook={handleAddBook} />
+      <div>
+        <input type="text" value={newBookTitle} onChange={(e) => setNewBookTitle(e.target.value)} />
+      </div>
+      <div>
+        <input type="text" value={newBookAuthor} onChange={(e) => setNewBookAuthor(e.target.value)} />
+      </div>
+      <button type="submit" onClick={handleAddBook}>Add Book</button>
     </div>
   );
 }
