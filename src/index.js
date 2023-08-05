cd8d20b571eb0ea06268e62813f9eeb9e46a5e8c
Sonika Kumar <sonikak004@gmail.com>
2023-08-05 03:42:06 +0100

    Add provider to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
+  document.getElementById('root'),
 );
