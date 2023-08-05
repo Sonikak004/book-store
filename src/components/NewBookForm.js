2876a63d60ebbcf4789c419d2f0b38308da46488
Sonika Kumar <sonikak004@gmail.com>
2023-08-05 19:40:13 +0100

    Remove the default authors and use api to store the data and delete them

--- src/components/NewBookForm.js
+++ src/components/NewBookForm.js
@@ -1,17 +1,24 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/booksSlice';
 
-function NewBookForm({ onAddBook }) {
+const NewBookForm = () => {
+  const dispatch = useDispatch();
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     const newBook = {
+      item_id: uuidv4(),
       title,
       author,
+      category: 'book',
     };
-    onAddBook(newBook);
+
+    dispatch(addBook(newBook));
     setTitle('');
     setAuthor('');
   };
@@ -19,19 +26,26 @@ function NewBookForm({ onAddBook }) {
   return (
     <form onSubmit={handleSubmit}>
       <div>
-        <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Title"
+        />
       </div>
       <div>
-        <input type="text" id="author" value={author} onChange={(e) => setAuthor(e.target.value)} />
+        <input
+          type="text"
+          id="author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+          placeholder="Author"
+        />
       </div>
-
       <button type="submit">Add Book</button>
     </form>
   );
-}
-
-NewBookForm.propTypes = {
-  onAddBook: PropTypes.func.isRequired,
 };
 
 export default NewBookForm;
